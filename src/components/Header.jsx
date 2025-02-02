9484b417d3c081bbbb3458803fe180cf42153858
Daniel <davidtea0427@gmail.com>
2025-02-02 17:50:08 +0800

    create header and banner

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,71 @@
+import logo from '@/assets/images/header/logo.png';
+import profile from '@/assets/images/header/profile.png';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { useState } from 'react';
+
+
 const Header = () => {
-  return (
-    <header>this is header</header>
-  )
+  const [toggle, setToggle] = useState(false);
+  
+
+
+  return (<>
+    <header className='d-lg-block d-none'>
+      <div className="container">
+        <nav className='py-16'>
+          <div className="justify-content-between d-flex align-items-center">
+            <div className='d-lg-block d-none'>
+              <Link to='/' className='me-40'>預約美麗時光</Link>
+              <Link to='/'>發現笑的可能</Link>
+            </div>
+            <div><img src={logo} width={210} height={80} alt="BeautiFull" /></div>
+            {/* <div className="dropdown">
+              <button className="border-0 bg-white dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" >
+              <img src={profile} width={40} height={40} classNameName='me-8' alt="profile" />使用者姓名
+              </button>
+              <ul className="dropdown-menu text-center rounded-0" style={{backgroundColor:'#F5DCD780'}}>
+                <li><Link to='/' className="dropdown-item" >帳戶資訊</Link></li>
+                <li><Link to='/' className="dropdown-item" >預約記錄</Link></li>
+              </ul>
+            </div> */}
+            <button className='btn' style={{backgroundColor:'#F5DCD780'}}>登入</button>
+          </div>
+        </nav>
+      </div>
+   
+
+    </header>
+
+    <header className='d-block d-lg-none'>
+      <div className="container">
+        <div className='d-flex justify-content-between align-items-center py-16'>
+          <div><img src={logo} width={210} height={80} alt="BeautiFull" /></div>
+          <div className={`d-lg-none d-block ${toggle ? 'd-none' : 'd-block'}`} onClick={() => setToggle(true)}>
+            <i className="bi bi-list fs-1"></i>
+          </div>
+          <div className={`${toggle ? 'd-block' : 'd-none'}`} onClick={() => setToggle(false)}>
+            <i className="bi bi-x-lg fs-3"></i>
+          </div>
+        </div>
+      </div>
+
+      {/* 下拉選單 */}
+      <div className={`bg-base-brown200 menu ${toggle ? 'open' : ''}`}>
+        <div className="container">
+          <div className='bg-base-brown200'>
+            <Link to='/' className='w-100 py-12 me-40 d-block'>預約美麗時光</Link>
+            <Link to='/' className='w-100 py-12 d-block'>發現笑的可能</Link>
+            <Link to='/' className='w-100 py-12 '>登入</Link>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+  </>)
 };
 export default Header;
+
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,53 @@
+import banner1 from '@/assets/images/banner/banner1.png'
+import banner2 from '@/assets/images/banner/banner2.png'
+import banner3 from '@/assets/images/banner/banner3.png'
+import bannerDeco from '@/assets/images/banner/bannerDeco.png'
+
 const Home = () => {
   return (
     <>
-      <h2>this is home page</h2>
+      <section className='py-lg-60 py-40 position-relative'>
+        <div className='banner-deco'></div>
+        <div className='banner-deco2 position-absolute top-50 left-0 translate-middle-y d-lg-block d-none'><img src={bannerDeco} width={156} height={132} alt="" /></div>
+        <div className="container d-lg-block d-none">
+          <div className='row'>
+            <div className="col-8 d-flex flex-column">
+              <div className='px-36 text-base-brown500'>
+                <h1 className='fw-bold' style={{fontSize:'80px'}}>BeautiFull</h1>
+                <p className='fs-3'>每一次呵護，都讓妳更接近理想中的自己</p>
+              </div>
+              <div className='text-end mt-auto'>
+                <img src={banner1} width={636} height={532} alt="" />
+              </div>
+            </div>
+
+            <div className='col-4'>
+              <div>
+                <img className='mb-24' src={banner2} width={416} height={520} alt="" />
+                <img src={banner3} width={416} height={236} alt="" />
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+
+        <div className="container d-block d-lg-none">
+          <div className="row">
+            <div className="col-12 text-base-brown500 mb-24">
+              <h1 className='fw-bold'>BeautiFull</h1>
+              <p className='fs-5'>每一次呵護，都讓妳更接近理想中的自己</p>
+            </div>
+            <div className="col-6">
+              <img className='mb-24' src={banner1} width={164} height={136} alt="" />
+              <img src={banner3} width={164} height={92} alt="" />
+            </div>
+            <div className='col-6'>
+              <img className='h-100' src={banner2} width={163} height={252} alt="" style={{objectFit:'cover'}} />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   )
 };
