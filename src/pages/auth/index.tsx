01c72b76c85562b85a45440ba78ca5903006815b
ruby10127130 <ruby10127130@gmail.com>
2025-02-12 23:12:16 +0800

    feat(auth): refactor page/auth with typescript, setup page partial loginMode and signupMode

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import "../auth/index.scss";
+import SignUpMode from './signupMode';
+import LoginMode from './loginMode';
+
+const PageAuthIndex = () => {
+  const [mode, setMode] = useState("login");
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [mode]);
+
+  return (
+    <>
+      <div className="auth-container">
+        {
+          mode === "login" && (
+            <LoginMode
+              onChangeMode={() => setMode("signup")} 
+              onSubmit={() => console.log('submit data') } 
+            />
+          )
+        }
+        {
+          mode === "signup" && (
+            <SignUpMode 
+              onChangeMode={() => setMode("login")} 
+              onSubmit={ () => console.log('submit data') }
+            />
+          )
+        }
+      </div>
+    </>
+  );
+};
+
+export default PageAuthIndex;
