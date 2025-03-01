8c3b2f6a4bc5b9f079efc77e139cf79157c079d1
ruby10127130 <ruby10127130@gmail.com>
2025-03-01 16:51:33 +0800

    build: setup react-hot-toast in root layout

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import ToasterProvider from "@/shared/Toaster";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 const DefaultLayout = () => {
   return (
     <>
+      <ToasterProvider />
       <Header />
-        <Outlet />
+      <Outlet />
       <Footer />
     </>
   );
